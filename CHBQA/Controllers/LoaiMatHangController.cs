82776112a19b9269b7859613dba608eb0d037c01
nguyentu1902 <sing19022001@gmail.com>
2022-03-23 22:42:27 +0700

    Trang Chu

---
+++ CHBQA/Controllers/LoaiMatHangController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CHBQA.Models;
+
+namespace CHBQA.Controllers
+{
+    public class LoaiMatHangController : Controller
+    {
+        // GET: LoaiMatHang
+        MyDataDataContext data = new MyDataDataContext();
+
+        public ActionResult Index()
+        {
+            var all_loaimh = from tt in data.LoaiHangs select tt;
+            return View(all_loaimh);
+        }
+    }
+}
---
+++ CHBQA/Controllers/MatHangController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CHBQA.Models;
+
+namespace CHBQA.Controllers
+{
+    public class MatHangController : Controller
+    {
+        // GET: MatHang
+        MyDataDataContext data = new MyDataDataContext();
+
+        public ActionResult ListSP()
+        {
+            var all_sp = from ss in data.SanPhams select ss;
+            return View(all_sp);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var matHang = data.SanPhams.Where(m => m.id_sp.Equals(id)).First();
+            return View(matHang);
+        }
+    }
+}
